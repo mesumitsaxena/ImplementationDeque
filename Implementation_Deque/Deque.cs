b3b8875c1c76bfa2aad20a6e86af9c612fe04236
unknown <sumit66saxena@gmail.com>
2022-02-09 16:04:42 +0530

    Adding initial Files

---
+++ Implementation_Deque/Deque.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation_Deque
+{
+    internal class Deque
+    {
+        public class DequeDLL
+        {
+            public int val;
+            public DequeDLL next;
+            public DequeDLL prev;
+            public DequeDLL(int x)
+            {
+                this.val = x;
+                this.next = null;
+                this.prev = null;
+            }
+        }
+        DequeDLL head;
+        DequeDLL tail;
+        public int Size;
+        public Deque()
+        {
+            head = new DequeDLL(-1);
+            tail = new DequeDLL(-1);
+            head.next = tail;
+            tail.prev = head;
+            Size = 0;
+        }
+        public void EnqueueRear(int x)
+        {
+            DequeDLL temp = new DequeDLL(x);
+            temp.prev = tail.prev;
+            temp.next = tail;
+            tail.prev.next = temp;
+            tail.prev = temp;
+            Size++;
+
+        }
+        public int DequeueRear()
+        {
+            if (Size > 0)
+            {
+                DequeDLL node = tail.prev;
+                tail.prev.prev.next = tail;
+                tail.prev = tail.prev.prev;
+                Size--;
+                return node.val;
+            }
+            return -1;
+
+        }
+        public int DequeueFront()
+        {
+            if (Size > 0)
+            {
+                DequeDLL node = head.next;
+                node.next.prev = head;
+                head.next = node.next;
+                Size--;
+                return node.val;
+            }
+            return -1;
+
+        }
+        public void EnqueueFront(int x)
+        {
+            DequeDLL temp = new DequeDLL(x);
+            head.next.prev = temp;
+            temp.prev = head;
+            temp.next = head.next;
+            head.next = temp;
+            Size++;
+        }
+        public int PeekRear()
+        {
+            if (Size > 0)
+            {
+                return tail.prev.val;
+            }
+            return -1;
+        }
+        public int PeekFront()
+        {
+            if (Size > 0)
+            {
+                return head.next.val;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Implementation_Deque/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Implementation_Deque;
+
+Console.WriteLine("Hello, World!");
+Deque obj = new Deque();
+obj.EnqueueRear(2);
+Console.WriteLine("Rear:"+obj.PeekRear());
+Console.WriteLine("Front:"+obj.PeekFront());
+Console.WriteLine("Size:" + obj.Size);
+Console.WriteLine("-----------------");
+obj.EnqueueRear(8);
+Console.WriteLine("Rear"+obj.PeekRear());
+Console.WriteLine("Front"+obj.PeekFront());
+Console.WriteLine("Size:" + obj.Size);
+Console.WriteLine("-----------------");
+obj.EnqueueFront(10);
+Console.WriteLine("Rear"+obj.PeekRear());
+Console.WriteLine("Front"+obj.PeekFront());
+Console.WriteLine("Size:" + obj.Size);
+Console.WriteLine("-----------------");
+obj.DequeueRear();
+Console.WriteLine("Rear" + obj.PeekRear());
+Console.WriteLine("Front" + obj.PeekFront());
+Console.WriteLine("Size:" + obj.Size);
+Console.WriteLine("-----------------");
+obj.EnqueueRear(200);
+Console.WriteLine("Rear" + obj.PeekRear());
+Console.WriteLine("Front" + obj.PeekFront());
+Console.WriteLine("Size:" + obj.Size);
+Console.WriteLine("-----------------");
+obj.DequeueFront();
+Console.WriteLine("Rear" + obj.PeekRear());
+Console.WriteLine("Front" + obj.PeekFront());
+Console.WriteLine("Size:" + obj.Size);
+Console.WriteLine("-----------------");
+
+
